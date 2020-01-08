64a6fffe69fdc00b11d216ce71e5ba38580bbed2
Kiet Tran <quang-kiet.tran@ekino.com>
2020-01-08 18:07:58 +0700

    Updates

---
+++ precache-manifest.6ec2c1b737dbe130aabf7333d233834f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce96a2034593f2a781dfded0d8cc897f",
+    "url": "/gigacover-assignment/index.html"
+  },
+  {
+    "revision": "6c92bdcdf940d2d53c65",
+    "url": "/gigacover-assignment/static/js/2.b1530f33.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/gigacover-assignment/static/js/2.b1530f33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "417d5b3342366db968bb",
+    "url": "/gigacover-assignment/static/js/main.9a2b4f70.chunk.js"
+  },
+  {
+    "revision": "b68f6243129c5e579202",
+    "url": "/gigacover-assignment/static/js/runtime-main.e9674f16.js"
+  },
+  {
+    "revision": "e6e9715ce9532e66e882b3ed49db9324",
+    "url": "/gigacover-assignment/static/media/loading.e6e9715c.svg"
+  }
+]);
